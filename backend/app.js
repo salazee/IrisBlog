b9dcc7160ebffbcea3afbb3a510a04407b8d987a
Zainab <134186742+salazee@users.noreply.github.com>
2026-02-12 18:57:36 +0100

    remove link

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app= express();
 app.use(express.json());
 
 app.use(cors({
-     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
