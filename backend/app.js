511ddbafe61e319724be6f14ebce4937c9ecdafc
Zainab <134186742+salazee@users.noreply.github.com>
2026-02-12 19:02:45 +0100

    add fall back

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app= express();
 app.use(express.json());
 
 app.use(cors({
-     origin: process.env.FRONTEND_URL,
+     origin: process.env.FRONTEND_URL||'https://irisblogbackend.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
